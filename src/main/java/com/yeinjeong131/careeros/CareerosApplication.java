756fccc3dc97b8c342504eaedbc917205a112ad2
Yein Jeong <bella060131@gmail.com>
2026-07-24 00:20:17 +1000

    feat: create Spring Boot project

---
+++ src/main/java/com/yeinjeong131/careeros/CareerosApplication.java
@@ -0,0 +1,13 @@
+package com.yeinjeong131.careeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerosApplication.class, args);
+	}
+
+}
